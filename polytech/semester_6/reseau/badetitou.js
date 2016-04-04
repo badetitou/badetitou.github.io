b36f4870a8147f10e6cd5d8c0187d2a9137817c3
Benoît Verhaeghe <badetitou@gmail.com>
2016-04-04 19:28:05 +0200

    Add Before

--- polytech/semester_6/reseau/badetitou.js
+++ polytech/semester_6/reseau/badetitou.js
@@ -3,6 +3,7 @@
  */
 
 var nbStation=2;
+var nbPort = 0;
 
 function addStation(m_routeAdresse, m_routeMasque, m_routePasserelle, m_ethport, m_ip4port) {
     nbStation++;
@@ -13,7 +14,7 @@ function addStation(m_routeAdresse, m_routeMasque, m_routePasserelle, m_ethport,
         object += "<div class=\"nom\">" + m_name + "</div>";
         object += "<div class=\"cadre_titre\" onClick=\"table_toggle($('#" + m_id + "_table'));\">Table de routage</div>";
         object += "<div class=\"tableroutage\" id=\"" + m_id + "_table\">";
-        for (var i = 0;i<m_routeAdresse.length;++i)
+        for (var i = 0;i<nbPort;++i)
         {
             object += "<div class=\"route\">";
                 object += "<div class=\"route_adresse\">" + m_routeAdresse[i] + "</div>";
@@ -37,4 +38,23 @@ function addStation(m_routeAdresse, m_routeMasque, m_routePasserelle, m_ethport,
     elements.push($('#' + m_id));
     connexions.push({'objet1': $('#station3_interface1'), 'objet2': $('#station2_interface1')});
     main();
+}
+
+function add_route_position() {
+    ++nbPort;
+
+    var object = "";
+    object += "<div class=\"mdl-textfield mdl-js-textfield mdl-textfield--floating-label\">";
+    object += "<input class=\"mdl-textfield__input\" type=\"text\" id=\"ask_station_route_address_"+nbPort+"\">";
+    object += "<label class=\"mdl-textfield__label\" for=\"ask_station_portIp6\">Route Adresse </label>";
+    object += "</div>";
+    object +="<div class=\"mdl-textfield mdl-js-textfield mdl-textfield--floating-label\">";
+    object += "<input class=\"mdl-textfield__input\" type=\"text\" id=\"ask_station_route_masque_"+nbPort+"\">";
+    object += "<label class=\"mdl-textfield__label\" for=\"ask_station_portIp6\">Route Masque</label>";
+    object += "</div>";
+    object += "<div class=\"mdl-textfield mdl-js-textfield mdl-textfield--floating-label\">";
+    object += "<input class=\"mdl-textfield__input\" type=\"text\" id=\"ask_station_route_passerelle_"+nbPort+"\">";
+    object += "<label class=\"mdl-textfield__label\" for=\"ask_station_portIp6\">Route Passerelle</label>";
+    object += "</div>";
+    $(".add_route_position").before(object);
 }
