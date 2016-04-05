2930c3a35600b96a315292eadf24d81151f15833
Benoît Verhaeghe <badetitou@gmail.com>
2016-04-05 19:14:46 +0200

    Add multi port...

--- polytech/semester_6/reseau/badetitou.js
+++ polytech/semester_6/reseau/badetitou.js
@@ -42,19 +42,59 @@ function addStation(m_routeAdresse, m_routeMasque, m_routePasserelle, m_ethport,
 
 function add_route_position() {
     ++nbPort;
-
-    var object = "";
-    object += "<div class=\"mdl-textfield mdl-js-textfield mdl-textfield--floating-label\">";
-    object += "<input class=\"mdl-textfield__input\" type=\"text\" id=\"ask_station_route_address_"+nbPort+"\">";
-    object += "<label class=\"mdl-textfield__label\" for=\"ask_station_portIp6\">Route Adresse </label>";
-    object += "</div>";
-    object +="<div class=\"mdl-textfield mdl-js-textfield mdl-textfield--floating-label\">";
-    object += "<input class=\"mdl-textfield__input\" type=\"text\" id=\"ask_station_route_masque_"+nbPort+"\">";
-    object += "<label class=\"mdl-textfield__label\" for=\"ask_station_portIp6\">Route Masque</label>";
-    object += "</div>";
-    object += "<div class=\"mdl-textfield mdl-js-textfield mdl-textfield--floating-label\">";
-    object += "<input class=\"mdl-textfield__input\" type=\"text\" id=\"ask_station_route_passerelle_"+nbPort+"\">";
-    object += "<label class=\"mdl-textfield__label\" for=\"ask_station_portIp6\">Route Passerelle</label>";
-    object += "</div>";
+    var object = document.createElement('div');
+    object.className = "mdl-textfield mdl-js-textfield mdl-textfield--floating-label";
+    object.id = "ask_station_route_address_nb_"+nbPort;
+    var input_route_address = document.createElement('input');
+    var label_route_address = document.createElement('label');
+    input_route_address.className = "mdl-textfield__input";
+    input_route_address.id = "ask_station_route_address_"+nbPort;
+    input_route_address.type = "text";
+    label_route_address.className = "mdl-textfield__label";
+    label_route_address.for = "ask_station_route_address_"+nbPort;
+    label_route_address.innerHTML = "Route Adresse";
+    object.appendChild(input_route_address);
+    object.appendChild(label_route_address);
+    componentHandler.upgradeElement(object);
     $(".add_route_position").before(object);
+
+    var object = document.createElement('div');
+    object.className = "mdl-textfield mdl-js-textfield mdl-textfield--floating-label";
+    object.id = "ask_station_route_masque_nb_"+nbPort;
+    var input_route_masque = document.createElement('input');
+    var label_route_masque = document.createElement('label');
+    input_route_masque.className = "mdl-textfield__input";
+    input_route_masque.id = "ask_station_route_masque_"+nbPort;
+    input_route_masque.type = "text";
+    label_route_masque.className = "mdl-textfield__label";
+    label_route_masque.for = "ask_station_route_masque_"+nbPort;
+    label_route_masque.innerHTML = "Route Masque";
+    object.appendChild(input_route_masque);
+    object.appendChild(label_route_masque);
+    componentHandler.upgradeElement(object);
+    $(".add_route_position").before(object);
+
+    var object = document.createElement('div');
+    object.className = "mdl-textfield mdl-js-textfield mdl-textfield--floating-label";
+    object.id = "ask_station_route_passerelle_nb_"+nbPort;
+    var input_route_passerelle = document.createElement('input');
+    var label_route_passerelle = document.createElement('label');
+    input_route_passerelle.className = "mdl-textfield__input";
+    input_route_passerelle.id = "ask_station_route_passerelle_"+nbPort;
+    input_route_passerelle.type = "text";
+    label_route_passerelle.className = "mdl-textfield__label";
+    label_route_passerelle.for = "ask_station_route_passerelle_"+nbPort;
+    label_route_passerelle.innerHTML = "Route Passerelle";
+    object.appendChild(input_route_passerelle);
+    object.appendChild(label_route_passerelle);
+    componentHandler.upgradeElement(object);
+    $(".add_route_position").before(object);
+}
+
+function remove_route_position(){
+    for (var i = nbPort; i > 0; i--){
+        $("#ask_station_route_passerelle_nb_"+i).remove();
+        $("#ask_station_route_address_nb_"+i).remove();
+        $("#ask_station_route_masque_nb_"+i).remove();
+    }
 }
