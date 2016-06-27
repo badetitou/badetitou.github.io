277c03178c570304fedd7b867ca1526a797e1412
Benoît "badetitou" Verhaeghe <badetitou@gmail.com>
2016-06-27 16:18:57 +0200

    Solve simple bug

--- Info/Reseau/badetitou.js
+++ Info/Reseau/badetitou.js
@@ -102,11 +102,11 @@ function add_route_position(dialog) {
     var input_route_address = document.createElement('input');
     var label_route_address = document.createElement('label');
     input_route_address.className = "mdl-textfield__input";
-    input_route_address.id = "ask_"+dialog+"_route_address_"+nbRoutage;
+    input_route_address.id = "ask_"+dialog+"_route_address_nb_"+nbRoutage;
     input_route_address.type = "text";
     input_route_address.pattern = "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}";
     label_route_address.className = "mdl-textfield__label";
-    label_route_address.for = "ask_"+dialog+"_route_address_"+nbRoutage;
+    label_route_address.for = "ask_"+dialog+"_route_address_nb_"+nbRoutage;
     label_route_address.innerHTML = "Route Adresse";
     error_route.className = "mdl-textfield__error";
     error_route.innerHTML = "Not IPV4 address";
@@ -124,11 +124,11 @@ function add_route_position(dialog) {
     var input_route_masque = document.createElement('input');
     var label_route_masque = document.createElement('label');
     input_route_masque.className = "mdl-textfield__input";
-    input_route_masque.id = "ask_"+dialog+"_route_masque_"+nbRoutage;
+    input_route_masque.id = "ask_"+dialog+"_route_masque_nb_"+nbRoutage;
     input_route_masque.type = "text";
     input_route_masque.pattern = "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}";
     label_route_masque.className = "mdl-textfield__label";
-    label_route_masque.for = "ask_"+dialog+"_route_masque_"+nbRoutage;
+    label_route_masque.for = "ask_"+dialog+"_route_masque_nb_"+nbRoutage;
     label_route_masque.innerHTML = "Route Masque";
     error_route.className = "mdl-textfield__error";
     error_route.innerHTML = "Not IPV4 address";
@@ -142,16 +142,16 @@ function add_route_position(dialog) {
     var error_route = document.createElement('span');
 
     object.className = "mdl-textfield mdl-js-textfield mdl-textfield--floating-label";
-    object.id = "ask_station_route_passerelle_nb_"+nbRoutage;
+    object.id = "ask_"+dialog+"_route_passerelle_nb_"+nbRoutage;
     var input_route_passerelle = document.createElement('input');
     var label_route_passerelle = document.createElement('label');
     input_route_passerelle.className = "mdl-textfield__input";
-    input_route_passerelle.id = "ask_" + dialog+"_route_passerelle_"+nbRoutage;
+    input_route_passerelle.id = "ask_" + dialog+"_route_passerelle_nb_"+nbRoutage;
     input_route_passerelle.type = "text";
     input_route_passerelle.pattern = "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}";
 
     label_route_passerelle.className = "mdl-textfield__label";
-    label_route_passerelle.for = "ask_"+dialog+"_route_passerelle_"+nbRoutage;
+    label_route_passerelle.for = "ask_"+dialog+"_route_passerelle_nb_"+nbRoutage;
     label_route_passerelle.innerHTML = "Route Passerelle";
 
     error_route.className = "mdl-textfield__error";
@@ -177,78 +177,6 @@ function remove_route_position(dialog){
 
 // Add port position
 
-function add_port_position() {
-    ++nbRoutage;
-
-    var object = document.createElement('div');
-    var error_route = document.createElement('span');
-    object.className = "mdl-textfield mdl-js-textfield mdl-textfield--floating-label";
-    object.id = "ask_station_route_address_nb_"+nbRoutage;
-    var input_route_address = document.createElement('input');
-    var label_route_address = document.createElement('label');
-    input_route_address.className = "mdl-textfield__input";
-    input_route_address.id = "ask_station_route_address_"+nbRoutage;
-    input_route_address.type = "text";
-    input_route_address.pattern = "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}";
-    label_route_address.className = "mdl-textfield__label";
-    label_route_address.for = "ask_station_route_address_"+nbRoutage;
-    label_route_address.innerHTML = "Route Adresse";
-    error_route.className = "mdl-textfield__error";
-    error_route.innerHTML = "Not IPV4 address";
-
-    object.appendChild(input_route_address);
-    object.appendChild(label_route_address);
-    object.appendChild(error_route);
-    componentHandler.upgradeElement(object);
-    $(".add_route_position").before(object);
-
-    var object = document.createElement('div');
-    var error_route = document.createElement('span');
-    object.className = "mdl-textfield mdl-js-textfield mdl-textfield--floating-label";
-    object.id = "ask_station_route_masque_nb_"+nbRoutage;
-    var input_route_masque = document.createElement('input');
-    var label_route_masque = document.createElement('label');
-    input_route_masque.className = "mdl-textfield__input";
-    input_route_masque.id = "ask_station_route_masque_"+nbRoutage;
-    input_route_masque.type = "text";
-    input_route_masque.pattern = "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}";
-    label_route_masque.className = "mdl-textfield__label";
-    label_route_masque.for = "ask_station_route_masque_"+nbRoutage;
-    label_route_masque.innerHTML = "Route Masque";
-    error_route.className = "mdl-textfield__error";
-    error_route.innerHTML = "Not IPV4 address";
-    object.appendChild(input_route_masque);
-    object.appendChild(label_route_masque);
-    object.appendChild(error_route);
-    componentHandler.upgradeElement(object);
-    $(".add_route_position").before(object);
-
-    var object = document.createElement('div');
-    var error_route = document.createElement('span');
-
-    object.className = "mdl-textfield mdl-js-textfield mdl-textfield--floating-label";
-    object.id = "ask_station_route_passerelle_nb_"+nbRoutage;
-    var input_route_passerelle = document.createElement('input');
-    var label_route_passerelle = document.createElement('label');
-    input_route_passerelle.className = "mdl-textfield__input";
-    input_route_passerelle.id = "ask_station_route_passerelle_"+nbRoutage;
-    input_route_passerelle.type = "text";
-    input_route_passerelle.pattern = "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}";
-
-    label_route_passerelle.className = "mdl-textfield__label";
-    label_route_passerelle.for = "ask_station_route_passerelle_"+nbRoutage;
-    label_route_passerelle.innerHTML = "Route Passerelle";
-
-    error_route.className = "mdl-textfield__error";
-    error_route.innerHTML = "Not IPV4 address";
-    object.appendChild(input_route_passerelle);
-    object.appendChild(label_route_passerelle);
-    object.appendChild(error_route);
-    componentHandler.upgradeElement(object);
-    $(".add_route_position").before(object);
+function add_port_position(dialog) {
+    ++nbPort;
 }
-
-/*
- *             Routeur
- */
-
