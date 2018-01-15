dad086ec3266597195811337261c423889b3c03e
badetitou <badetitou@gmail.com>
2018-01-15 21:01:02 +0100

    try build.sh

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,12 +1,36 @@
-# Keep Travis from testing `master` branch
-branches:
-  except:
-  - master
+#!/bin/bash
 
-# Deployment config
-deploy:
-  provider: script
-  script: "./_scripts/build.sh"
-  skip_cleanup: true
-  on:
-    branch: source
+# Enable error reporting to the console.
+set -e
+
+# Install bundles if needed.
+bundle check || bundle install
+
+# NPM install if needed.
+. $HOME/.nvm/nvm.sh && nvm install 6.1 && nvm use 6.1
+npm install
+
+# Build the site.
+gulp
+
+# Checkout `master` and remove everything.
+git clone https://${GH_TOKEN}@github.com/badetitou/badetitou.github.io.git ../badetitou.github.io.master
+cd ../badetitou.github.io.master
+git checkout master
+rm -rf *
+
+# Copy generated HTML site from source branch in original repository.
+# Now the `master` branch will contain only the contents of the _site directory.
+cp -R ../badetitou.github.io/_site/* .
+
+# Make sure we have the updated .travis.yml file so tests won't run on master.
+cp ../badetitou.github.io/.travis.yml .
+git config user.email ${GH_EMAIL}
+git config user.name "badetitou-bot"
+
+# Commit and push generated content to `master` branch.
+git status
+git add -A .
+git status
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git push --quiet origin `master` > /dev/null 2>&1
