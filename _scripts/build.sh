8824a08f8b80a18607b4a41f96f7755df2154219
badetitou <badetitou@gmail.com>
2018-01-15 21:26:25 +0100

    trest 5

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
-# skip if build is triggered by pull request
-if [ $TRAVIS_PULL_REQUEST == "true" ]; then
-  echo "this is PR, exiting"
-  exit 0
-fi
-
 # enable error reporting to the console
 set -e
 
